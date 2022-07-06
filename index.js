852c8499447b783b5d2631516a23428203a5098a
Thomas K <thmskffr@gmail.com>
2022-07-07 01:18:33 +0200

    working on progress bar

--- index.js
+++ index.js
@@ -10,6 +10,33 @@ document.addEventListener("click", e => {
     }
 })
 
+window.addEventListener("resize", (e) => {
+    //Recalculate Progress Bar
+})
+
+document.querySelectorAll(".progress-bar").forEach(calculateProgressBar)
+
+const calculateProgressBar = (progressBar) => {
+    progressBar.innerHTML = ""
+    const slider = progressBar.closest(".row").querySelector("slider")
+
+    const itemCount = slider.children.length
+    const itemsPerScreen = pareInt(getComputedStyle(slider).getPropertyValue("--items-per-screen"))
+
+    const progressBarItemCount = Math.ceil(itemsPerScreen / itemCount)
+
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
+
+    for (let i = 0; i < progressBarItemCount; i++) {
+        const barItem = document.createElement("div")
+        barItem.classList.add("progress-item")
+        if (i === sliderIndex) {
+            barItem = classList.add("active")
+        }
+        progressBar.append(barItem)
+    }
+}
+
 const onHandleClick = (handle) => {
     const slider = handle.closest(".container").querySelector(".slider")
     const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
@@ -20,3 +47,4 @@ const onHandleClick = (handle) => {
         slider.style.setProperty("--slider-index", sliderIndex + 1)
     }
 }
+
