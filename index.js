e4014e9cc4166f53edd46f7b8841c0d0450af9d4
Thomas K <thmskffr@gmail.com>
2022-07-07 15:08:47 +0200

    slider ok, needs work on looping

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+window.addEventListener("resize", (e) => {
+    //Recalculate Progress Bar
+})
+
+const calculateProgressBar = (progressBar) => {
+    progressBar.innerHTML = ""
+    const slider = progressBar.closest(".row").querySelector(".slider")
+
+    const itemCount = slider.children.length
+    const itemsPerScreen = parseInt(getComputedStyle(slider).getPropertyValue("--items-per-screen"))
+
+    const progressBarItemCount = Math.ceil(itemCount /itemsPerScreen)
+
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
+
+    for (let i = 0; i < progressBarItemCount; i++) {
+        const barItem = document.createElement("div")
+        console.log(barItem.classList)
+        barItem.classList.add("progress-item")
+
+        if (i === sliderIndex) {
+            barItem.classList.add("active")
+        }
+        progressBar.append(barItem)
+    }
+}
+
+document.querySelectorAll(".progress-bar").forEach(calculateProgressBar)
+
+
+const onHandleClick = (handle) => {
+const progressBar = handle.closest(".row").querySelector(".progress-bar")
+
+    const slider = handle.closest(".container").querySelector(".slider")
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
+    if (handle.classList.contains("left-handle")) {
+        slider.style.setProperty("--slider-index", sliderIndex - 1)
+        progressBar.children[sliderIndex].classList.remove("active")
+        progressBar.children[sliderIndex - 1].classList.add("active")
+    }
+    if (handle.classList.contains("right-handle")) {
+        slider.style.setProperty("--slider-index", sliderIndex + 1)
+        progressBar.children[sliderIndex].classList.remove("active")
+        progressBar.children[sliderIndex + 1].classList.add("active")
+    }
+}
+
 document.addEventListener("click", e => {
     let handle;
     if (e.target.matches(".handle")) {
@@ -9,42 +56,3 @@ document.addEventListener("click", e => {
         onHandleClick(handle)
     }
 })
-
-window.addEventListener("resize", (e) => {
-    //Recalculate Progress Bar
-})
-
-document.querySelectorAll(".progress-bar").forEach(calculateProgressBar)
-
-const calculateProgressBar = (progressBar) => {
-    progressBar.innerHTML = ""
-    const slider = progressBar.closest(".row").querySelector("slider")
-
-    const itemCount = slider.children.length
-    const itemsPerScreen = pareInt(getComputedStyle(slider).getPropertyValue("--items-per-screen"))
-
-    const progressBarItemCount = Math.ceil(itemsPerScreen / itemCount)
-
-    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
-
-    for (let i = 0; i < progressBarItemCount; i++) {
-        const barItem = document.createElement("div")
-        barItem.classList.add("progress-item")
-        if (i === sliderIndex) {
-            barItem = classList.add("active")
-        }
-        progressBar.append(barItem)
-    }
-}
-
-const onHandleClick = (handle) => {
-    const slider = handle.closest(".container").querySelector(".slider")
-    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
-    if (handle.classList.contains("left-handle")) {
-        slider.style.setProperty("--slider-index", sliderIndex - 1)
-    }
-    if (handle.classList.contains("right-handle")) {
-        slider.style.setProperty("--slider-index", sliderIndex + 1)
-    }
-}
-
